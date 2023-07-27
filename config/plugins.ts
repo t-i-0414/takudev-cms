3bd7b02dc316aea907f070cdbc5943a23283c67c
t-i-0414 <gutlo.ol0414@gmail.com>
2023-07-27 09:53:38 +0900

    fix: 🐛 plugins.ts

---
+++ config/plugins.ts
@@ -0,0 +1,18 @@
+export default ({ env }) => ({
+  upload:
+    process.env.NODE_ENV === "development"
+      ? {}
+      : {
+          config: {
+            provider: "aws-s3",
+            providerOptions: {
+              accessKeyId: env("AWS_ACCESS_KEY_ID"),
+              secretAccessKey: env("AWS_ACCESS_SECRET"),
+              region: env("AWS_REGION"),
+              params: {
+                Bucket: env("AWS_BUCKET_NAME"),
+              },
+            },
+          },
+        },
+});
