c30c96a723ddcb65b6b4c510b13890b70304aa93
t-i-0414 <gutlo.ol0414@gmail.com>
2023-07-29 21:39:53 +0000

    fix: 🐛 s3 provider config

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,13 +5,23 @@ export default ({ env }) => ({
           config: {
             provider: "aws-s3",
             providerOptions: {
-              accessKeyId: env("AWS_ACCESS_KEY_ID"),
-              secretAccessKey: env("AWS_ACCESS_SECRET"),
-              region: env("AWS_REGION"),
-              params: {
-                ACL: "private",
-                Bucket: env("AWS_BUCKET_NAME"),
+              baseUrl: env("CDN_URL"),
+              rootPath: env("CDN_ROOT_PATH", ""),
+              s3Options: {
+                accessKeyId: env("AWS_ACCESS_KEY_ID"),
+                secretAccessKey: env("AWS_ACCESS_SECRET"),
+                region: env("AWS_REGION"),
+                params: {
+                  ACL: "private",
+                  signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+                  Bucket: env("AWS_BUCKET_NAME"),
+                },
               },
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
             },
           },
         }
