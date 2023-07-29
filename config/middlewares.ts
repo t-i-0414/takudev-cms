ca4d1d4ea05ee6475b402a382f3f1ada7e14f786
t-i-0414 <gutlo.ol0414@gmail.com>
2023-07-29 16:46:59 +0000

    refactor: 💡 s3 provider config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,39 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  process.env.NODE_ENV === "production"
+    ? {
+        name: "strapi::security",
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              "connect-src": ["'self'", "https:"],
+              "img-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "market-assets.strapi.io",
+                "takudev-images.s3.ap-northeast-1.amazonaws.com",
+              ],
+              "media-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "market-assets.strapi.io",
+                "takudev-images.s3.ap-northeast-1.amazonaws.com",
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      }
+    : "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
